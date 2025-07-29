d6581f6a5109c9cf7e1f1caa5379610fedf9117f
amila-thushantha <thushantha680@gmail.com>
2025-07-29 15:33:37 +0530

    HeroSection created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { GiHamburgerMenu} from 'react-icons/gi';
+import {data} from '../restApi.json';
+
+
+const Navbar = () => {
+    const [show, setShow] = useState(false)
+  return (
+    <nav>
+
+        <div className="logo">AMIII</div>
+        <div className={show ? "navLinks showmenu":"navLinks"}>
+            <div className="links">
+                {
+                    data[0].navbarLinks.map(element =>{
+                        return(
+                            <Link 
+                            to={element.link} 
+                            key={element.id} 
+                            spy={true} smooth={true} 
+                            duration={500} 
+                            >
+                                {element.title}
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+        </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Success.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Success() {
+  return (
+    <>
+      <h1>Success page</h1>
+    </>
+  )
+}
+
+export default Success
